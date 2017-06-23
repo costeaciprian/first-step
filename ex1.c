977f3c39b3d9a7d5974ea564078212ab3672c2d2
Costea Ciprian <costea_ciprian07@yahoo.com>
2017-06-23 16:42:30 +0300

    Added first ex

---
+++ ex1.c
@@ -0,0 +1,20 @@
+#include <stdio.in>
+#include <stdlib.h>
+
+#define SIZE 1000
+#define LIMIT 399
+
+int main() {
+
+	int **mat = malloc(SIZE * sizeof(int *));
+	for( int i = 0; i < SIZE; i++) {
+		mat[i] = malloc(SIZE * sizeof(int));
+	}
+
+	for(i = 0; i < SIZE; i++) {
+		mat[i] = rand() % LIMIT;
+		mat[i] = mat[i] << 2; 
+	}
+
+	return 0;
+}
